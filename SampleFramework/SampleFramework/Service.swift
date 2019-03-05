1b1217e7e9aa7bbb233d55153e019135a454c663
Dipa Tapsale <ghorpade.dipa@gmail.com>
2019-03-05 16:44:15 +0530

    Initial commit

---
+++ SampleFramework/SampleFramework/Service.swift
@@ -0,0 +1,15 @@
+//
+//  Service.swift
+//  SampleFramework
+//
+//  Created by Dipa Tapsale on 05/03/19.
+//  Copyright © 2019 Dipa Tapsale. All rights reserved.
+//
+
+import Foundation
+class Service{
+    private init(){}
+    public static func doSomething() -> String{
+        return "Hello World"
+    }
+}
